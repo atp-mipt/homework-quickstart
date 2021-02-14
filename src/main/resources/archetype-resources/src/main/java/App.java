2c25a9ffdd8fda1f909475caf6842e899e49b678
inponomarev <iponomarev@mail.ru>
2021-02-14 13:44:55 +0300

    initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,10 @@
+package $package;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+package $package;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+public class AppTest {
+    @Test
+    void helloTest() {
+        Assertions.assertThat(2 + 2).isEqualTo(4);
+    }
+}
